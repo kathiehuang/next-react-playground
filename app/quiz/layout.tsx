1b0eeef56fd7e9440b5a7d8208904756323714dd
Kathie Huang <kathiehuang@hotmail.com>
2024-01-02 11:46:48 -0800

    Initial commit

---
+++ app/quiz/layout.tsx
@@ -0,0 +1,10 @@
+export default function NestedLayout({ children, }: {
+    children: React.ReactNode;
+}) {
+    return (
+        <nav>
+            This is my navigation
+            {children}
+        </nav>
+    )
+}
